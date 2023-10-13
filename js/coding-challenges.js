e9c762f507856002986927eef8ba2279c64d5286
Robert Owens <robertowens95@gmail.com>
2023-10-13 10:57:43 -0500

    solved problem #1 of intermediate challenges

--- js/coding-challenges.js
+++ js/coding-challenges.js
@@ -16,46 +16,46 @@ Objective: Write a script to generate and output the first 25 narcissistic integ
 // return the array into a string or number then add the 3 of them up
 // if they add up to the input then it is a narcissisticNum.
 
-const isNarcissisticNum = (n) => {
-  //   const lengthOfN = n.length;
-  let numString;
-  let toArray;
-  if (isNaN(n)) {
-    //validation to determine if n is a numerical value
-    return console.log("Please enter a valid number.");
-  } else if (!isNaN(n) && typeof n === "number") {
-    // if it is a numerical value and it is a number
-    numString = n.toString(); // convert to a string
-    console.log("this is a string now", numString);
-    let result = "";
-    for (let i = 0; i < numString.length; i++) {
-      // loop through the string to add a comma between each char.
-      result += numString[i];
-      if (i < numString.length - 1) {
-        result += ",";
-      }
-    }
-    let newResult = []; //set empty array as a variable
-    toArray = result.split(","); //turning the string into an array by splitting.
-    console.log("This is back to an array", toArray);
-    for (let j = 0; j < toArray.length; j++) {
-      // looping the array to power each num in the array by the length of the original input
-      newResult.push(Math.pow(toArray[j], numString.length));
-    }
-    console.log("This should return cubed of each number in array", newResult);
-    let addedAmount = newResult[0] + newResult[1] + newResult[2]; // adding each result together.
-    console.log("The added amount is:", addedAmount);
-    if (addedAmount === n) {
-      // logic to see if the result is a narcissistic number
-      return console.log("This is a narcissistic number.");
-    } else {
-      return console.log("This is not a narcissistic number.");
-    }
-  }
-  return;
-};
+// const isNarcissisticNum = (n) => {
+//   //   const lengthOfN = n.length;
+//   let numString;
+//   let toArray;
+//   if (isNaN(n)) {
+//     //validation to determine if n is a numerical value
+//     return console.log("Please enter a valid number.");
+//   } else if (!isNaN(n) && typeof n === "number") {
+//     // if it is a numerical value and it is a number
+//     numString = n.toString(); // convert to a string
+//     console.log("this is a string now", numString);
+//     let result = "";
+//     for (let i = 0; i < numString.length; i++) {
+//       // loop through the string to add a comma between each char.
+//       result += numString[i];
+//       if (i < numString.length - 1) {
+//         result += ",";
+//       }
+//     }
+//     let newResult = []; //set empty array as a variable
+//     toArray = result.split(","); //turning the string into an array by splitting.
+//     console.log("This is back to an array", toArray);
+//     for (let j = 0; j < toArray.length; j++) {
+//       // looping the array to power each num in the array by the length of the original input
+//       newResult.push(Math.pow(toArray[j], numString.length));
+//     }
+//     console.log("This should return cubed of each number in array", newResult);
+//     let addedAmount = newResult[0] + newResult[1] + newResult[2]; // adding each result together.
+//     console.log("The added amount is:", addedAmount);
+//     if (addedAmount === n) {
+//       // logic to see if the result is a narcissistic number
+//       return console.log("This is a narcissistic number.");
+//     } else {
+//       return console.log("This is not a narcissistic number.");
+//     }
+//   }
+//   return;
+// };
 
-isNarcissisticNum(370);
+// isNarcissisticNum(370);
 
 /*
 TODO:
@@ -69,7 +69,38 @@ example:
 Input: start: 1 finish: 4 step: 1 | Output: 10
 */
 
-const summedSequence = (start, finish, step) => {
-  let result;
-  for (start = 0; start <= finish; step++) {}
+// const summedSequence = (start, finish, step) => {
+//   let result;
+//   for (start = 0; start <= finish; step++) {}
+// };
+
+/* 
+Write a function that takes an array of integers and a target integer, then returns the indices of the two integers that add up to the target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Input: [ 1, 4, 8, 12 ], 20 | Output: [ 2, 3 ]
+*/
+
+/*
+first loop through the array to find the biggest numbers
+add 2 elements together to see if they = the target using multiple if statements
+output the indices that = to the target.
+*/
+
+const indices = (arr, target) => {
+  let sum;
+  let indices;
+  let string;
+  arr.forEach((value, index) => {
+    for (let i = index + 1; i < arr.length; i++) {
+      if (value + arr[i] === target) {
+        sum = [value, arr[i]];
+        indices = [arr.indexOf(sum[0]), arr.indexOf(sum[1])];
+        break;
+      }
+    }
+  });
+  return indices;
 };
+
+const testArr = [1, 4, 8, 12];
+
+console.log(indices(testArr, 20));
