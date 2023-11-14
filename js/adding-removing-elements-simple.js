201bdef4c26d1595b426dff0237934d9e7b693ef
Robert Owens <robertowens95@gmail.com>
2023-11-13 20:15:02 -0600

    dom traverse exercies

--- js/adding-removing-elements-simple.js
+++ js/adding-removing-elements-simple.js
@@ -6,6 +6,12 @@
         createBox.classList.add("box");
         //select where you want to put on the dom
         const boxContainer = document.querySelector("#boxes");
+        //adding a number increment to each box
+        createBox.innerHTML = `
+        <span>${document.querySelectorAll(".box").length + 1}</span>`;
+        createBox.addEventListener("click", (e) => {
+            createBox.remove();
+        });
         //add the node to the parent
         boxContainer.appendChild(createBox);
     });
---
+++ js/to-do-list.js
@@ -0,0 +1,40 @@
+/**
+ *  <li class="to-do-item list-group-item d-flex justify-content-between align-items-center">
+    <p class="m-0">Finish this exercise</p>
+    <button class="btn btn-danger">Done</button>
+    </li>
+ */
+
+const todoListHandler = () => {
+    const todo = document.createElement("li");
+    const doneBtnLength = document.querySelectorAll(".btn-danger").length;
+    const todoInput = document.querySelector("#to-do").value;
+    todo.classList.add(
+        "to-do-item",
+        "list-group-item",
+        "d-flex",
+        "justify-content-between",
+        "align-items-center"
+    );
+    todo.innerHTML = `
+        <p class="m-0">${todoInput}</p>
+    <button class="btn btn-danger" id="done${doneBtnLength}">Done</button>
+    `;
+    document.querySelector("#to-do-list").append(todo);
+
+    const doneBtn = document.querySelector(`#done${doneBtnLength}`);
+    const removeListHandler = (event) => {
+        doneBtn.parentElement.remove();
+    };
+
+    doneBtn.addEventListener("click", removeListHandler);
+};
+
+//Main
+(() => {
+    const addTodoBtn = document.querySelector("#addToDo");
+    addTodoBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        todoListHandler();
+    });
+})();
