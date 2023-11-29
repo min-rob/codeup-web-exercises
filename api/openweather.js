d216a20d509d00159b67c2b81736596470c94f33
Robert Owens <robertowens95@gmail.com>
2023-11-29 15:33:39 -0600

    finished weather proj

--- api/openweather.js
+++ api/openweather.js
@@ -5,12 +5,16 @@ import { keys } from "../js/keys.js";
  * @param {number} lng  - longitude of the coordinates
  * @returns {Promise} - a promise that resolves to the forecast data
  */
+
+const OPEN_WEATHER_API_KEY = keys.openWeather;
+const OPEN_WEATHER_API_KEY2 = keys.openWeather2;
+
 export const getForecast = async (lat, lng) => {
     if (Array.isArray(lat)) {
         lng = lat[1];
         lat = lat[0];
     }
-    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&units=imperial&appid=${keys.openweather}`;
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&units=imperial&appid=${OPEN_WEATHER_API_KEY}`;
     const options = {
         method: "GET",
     };
--- js/weather_map.js
+++ js/weather_map.js
@@ -2,10 +2,10 @@ import { keys } from "./keys.js";
 import { getForecast } from "../api/openweather.js";
 import { createMap, getCoordinates, getAddress } from "../api/mapbox.js";
 
-const OPEN_WEATHER_API_KEY = keys.openWeather;
 const MAPBOX_API_KEY = keys.mapBox;
 let sessionToken = null;
 let selectedSuggestion = {};
+let userCoord = [];
 
 const generateSessionToken = () => {
     return Math.random().toString(36).substring(7);
@@ -30,10 +30,10 @@ const saveSuggestionClick = (suggestion) => {
     return selectedSuggestion;
 };
 
-const handleSearchBar = (callback) => {
-    const suggestionList = document.querySelector("#suggestions");
-    const searchBar = document.querySelector("input[type='search']");
+const searchBar = document.querySelector("input[type='search']");
+const suggestionList = document.querySelector("#suggestions-list");
 
+const handleSearchSuggestion = (callback) => {
     searchBar.addEventListener("keyup", async (e) => {
         const userInput = e.target.value.toLowerCase();
         suggestionList.innerHTML = "";
@@ -53,7 +53,7 @@ const handleSearchBar = (callback) => {
                     const li = document.createElement("li");
                     li.classList.add("suggestion");
                     li.textContent = suggestion.name;
-                    li.addEventListener("click", () => {
+                    li.addEventListener("click", async (e) => {
                         // Call the function with the suggestion object
                         saveSuggestionClick(suggestion);
                         console.log("Returned suggestion:", suggestion);
@@ -69,8 +69,21 @@ const handleSearchBar = (callback) => {
     });
 };
 
+const handleSearch = async (callback) => {
+    const form = document.querySelector("#search-form");
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const searchValue = searchBar.value;
+        console.log("Search value:", searchValue);
+        const searchCoords = await getCoordinates(searchValue);
+        const searchAddress = await getAddress(searchCoords);
+        suggestionList.innerHTML = "";
+        console.log("search address:", searchAddress);
+        callback(searchAddress); // Pass the result to the callback function
+    });
+};
+
 const userLocation = (callback) => {
-    let userCoord = [];
     // Check if geolocation is supported by the browser
     if ("geolocation" in navigator) {
         // Prompt user for permission to access their location
@@ -82,7 +95,7 @@ const userLocation = (callback) => {
                 const lng = position.coords.longitude;
                 userCoord.push(lng, lat);
                 callback(userCoord);
-                console.log(userCoord);
+                console.log("Current location of user:", userCoord);
             },
             // Error callback function
             (error) => {
@@ -98,7 +111,7 @@ const userLocation = (callback) => {
 
 const setLocation = async (address = [-77.60706, 38.78828]) => {
     const newAddress = await getAddress(address);
-    console.log(newAddress);
+    console.log("User location", newAddress);
     const city = newAddress.features.find((feature) =>
         feature.place_type.includes("place")
     ).text;
@@ -109,7 +122,7 @@ const setLocation = async (address = [-77.60706, 38.78828]) => {
     const locationText = document.querySelector("#user-location");
     locationText.textContent = `${city}, ${state}`;
     const forecastLocation = document.querySelector("#forecast-location");
-    forecastLocation.textContent = `Forcast in: ${city}, ${state}`;
+    forecastLocation.textContent = `5 Day forecast: ${city}, ${state}`;
 };
 
 const updateTime = () => {
@@ -127,11 +140,122 @@ const updateTime = () => {
     document.querySelector("#time").innerText = formattedTime;
 };
 
+const weatherForecast = async (coordinates) => {
+    const forecast = await getForecast(coordinates[1], coordinates[0]);
+    return forecast;
+};
+
+const dateFormatter = (dateTime) => {
+    const oldDate = new Date(dateTime * 1000);
+    const date = new Date(parseFloat(dateTime) * 1000);
+    const currentDate = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const dayOfWeek = date.toLocaleDateString("en-US", {
+        weekday: "long",
+    });
+    const day = date.getDate();
+    const month = date.toLocaleDateString("en-US", { month: "short" });
+    const year = date.getFullYear();
+
+    const oldDateDay = oldDate.getDate();
+    const currentDateDay = currentDate.getDate();
+
+    let formattedDate = `${dayOfWeek} ${month} ${day}, ${year}`;
+
+    if (oldDateDay === currentDateDay) {
+        formattedDate = "Today";
+    }
+    return formattedDate;
+};
+
+const createWeatherCard = (forecast) => {
+    let { weather, temp, humidity, wind_speed, pressure, dateTime } = forecast;
+    let imgUrl = "";
+
+    weather = forecast.weather[0].main;
+    temp = Math.trunc(forecast.temp.day);
+    humidity = forecast.humidity;
+    wind_speed = forecast.wind_speed;
+    pressure = forecast.pressure;
+    dateTime = forecast.dt;
+    const newDateTime = dateFormatter(dateTime);
+
+    switch (weather) {
+        case "Clouds":
+            imgUrl = "./weatherbox_assets/img/cloud.png";
+            break;
+        case "Rain":
+            imgUrl = "./weatherbox_assets/img/rainy.png";
+            break;
+        case "Clear":
+            imgUrl = "./weatherbox_assets/img/sun.png";
+            break;
+        case "Snow":
+            imgUrl = "./weatherbox_assets/img/snowy.png";
+            break;
+        case "Thunderstorm":
+            imgUrl = "./weatherbox_assets/img/storm.png";
+            break;
+        case "windy":
+            imgUrl = "./weatherbox_assets/img/windy.png";
+            break;
+        default:
+            // Handle other cases
+            break;
+    }
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("weather-card");
+    card.innerHTML = `
+            <div
+            class="card-head d-flex justify-content-between"
+        >
+            <p><strong>${newDateTime}</strong></p>
+        </div>
+        <div class="card-body">
+            <div class="d-flex flex-column align-items-center justify-content-center">
+                <h1 class="temp">${temp}°C</h1>
+                <img
+                    src= ${imgUrl}
+                    height="125"
+                    width="125"
+                />
+                <h2>${weather}</h2>
+            </div>
+        </div>
+        <div class="card-footer">
+            <ul>
+                <li>
+                    <span>Humidity: ${humidity}</span>
+                </li>
+                <li>
+                    <span>Wind speed: ${wind_speed} Mph</span>
+                </li>
+                <li><span>Pressure: ${pressure}</span></li>
+            </ul>
+        </div>`;
+    return card;
+};
+
+const renderWeatherCard = (forecasts) => {
+    forecasts = forecasts.daily.slice(0, 5);
+    const parentDiv = document.querySelector(".weather-card-container");
+    parentDiv.innerHTML = "";
+
+    const weatherFragment = document.createDocumentFragment();
+    for (let forecast of forecasts) {
+        weatherFragment.appendChild(createWeatherCard(forecast));
+    }
+    parentDiv.appendChild(weatherFragment);
+};
+
 (async () => {
     updateTime();
     setInterval(updateTime, 1000);
-    const map = createMap("map", [-77.60706, 38.78828]);
-    handleSearchBar(async (suggestion) => {
+    const map = createMap("map", [-98.45408988902976, 29.519240323311802]);
+    handleSearchSuggestion(async (suggestion) => {
         const coordinates = await getCoordinates(suggestion.full_address);
         map.flyTo({
             center: coordinates,
@@ -149,9 +273,44 @@ const updateTime = () => {
         const state = suggestion.context.region.name;
         console.log("new city/state", city, state);
         const forecastLocation = document.querySelector("#forecast-location");
-        forecastLocation.textContent = `Forecast in: ${city}, ${state}`;
+        forecastLocation.textContent = `Weather: ${city}, ${state}`;
+        const forecast = await weatherForecast(coordinates);
+        console.log("the forecast obj", forecast.daily);
+        renderWeatherCard(forecast);
     });
-    userLocation((coordinates) => {
+    handleSearch(async (address) => {
+        const coordinates = address.features[0].center;
+        map.flyTo({
+            center: coordinates,
+            zoom: 15,
+            speed: 2,
+        });
+        const popup = new mapboxgl.Popup().setHTML(
+            `<p>${address.features[0].place_name}</p>`
+        );
+        const marker = new mapboxgl.Marker({
+            draggable: true,
+        })
+            .setLngLat(coordinates)
+            .addTo(map)
+            .setPopup(popup);
+        const city = address.features.find((feature) =>
+            feature.place_type.includes("place")
+        ).text;
+        const state = address.features.find((feature) =>
+            feature.place_type.includes("region")
+        ).text;
+        console.log("new city/state", city, state);
+        const forecastLocation = document.querySelector("#forecast-location");
+        forecastLocation.textContent = `Weather: ${city}, ${state}`;
+        const forecast = await weatherForecast(coordinates);
+        console.log("the forecast obj", forecast.daily);
+        renderWeatherCard(forecast);
+    });
+
+    userLocation(async (coordinates) => {
         setLocation(coordinates);
+        const userForecast = await weatherForecast(coordinates);
+        renderWeatherCard(userForecast);
     });
 })();
